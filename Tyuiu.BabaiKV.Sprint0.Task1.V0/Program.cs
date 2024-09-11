12d12cd2169be3665f90319f56782ea2aff0a3bd
Tyuiu.BabaiKV@outlook.com <Tyuiu.BabaiKV@outlook.com>
2024-09-11 22:25:13 +0500

    Task1

---
+++ Tyuiu.BabaiKV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.BabaiKV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Ксения";
+            int age = 18;
+            Console.WriteLine("Здравствуйте! Меня зовут " + name + ", мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
