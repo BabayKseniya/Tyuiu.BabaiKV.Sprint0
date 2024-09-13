2bfa2216f4dfc48e136b65709116776f46771abb
Tyuiu.BabaiKV@outlook.com <Tyuiu.BabaiKV@outlook.com>
2024-09-13 21:20:08 +0500

    Task3

---
+++ Tyuiu.BabaiKV.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.BabaiKV.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.BabaiKV.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
